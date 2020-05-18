9729c937de24b33dc85f6a73027073b78249586a
JenPiwonskiGA <jenpiwonski@gmail.com>
2020-05-18 19:01:54 -0400

    authentication complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, length: { minimum: 6 }
+
+  def frontend_data
+    { 
+      id: id, 
+      email:email, 
+      created_at: created_at,
+      updated_at: updated_at
+    }
+  end
 end
