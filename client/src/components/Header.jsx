f97885acbad5f8726b56d963b9051bd04173496f
Lia Kapanadze <liakapanadze@Lias-MacBook.fios-router.home>
2020-05-20 12:59:36 -0400

    initial styling

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import Nav from './Nav'
-import { ReactComponent as Logo } from '../services/dd_icon_rgb.svg'
+import { ReactComponent as Logo } from '../services/dd_logo_h_white.svg'
+import { Link } from 'react-router-dom'
 
 export default function Header(props) {
   return (
     <div className='header'>
-      <h1>CoPaws</h1>
-      <Logo/>
+      <Link to='/'>
+        <Logo className='header-logo' />
+      </Link>
 
       {!props.currentUser ?
-        ''
+        <div className='public-buttons'>
+          <Link to='/login' className='login-link'>Already Have an Account?</Link>
+          <Link to='/register' ><button className='register-button'>Sign Up</button></Link>
+        </div>
+
         :
         <div className='logged-in-header'>
           <Nav />
