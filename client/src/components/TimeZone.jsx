2d89a7061db616afd8f799171ce2a98a815d391b
JenPiwonskiGA <jenpiwonski@gmail.com>
2020-05-20 11:44:45 -0400

    quick fix

--- client/src/components/TimeZone.jsx
+++ client/src/components/TimeZone.jsx
@@ -6,7 +6,7 @@ export default class TimeZone extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      timezone:[]
+      timezone:''
     }
     console.log(Timezones)
   }
