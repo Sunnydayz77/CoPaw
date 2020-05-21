7911dd84b6473f2e171958c224ae385799711019
JenPiwonskiGA <jenpiwonski@gmail.com>
2020-05-21 00:15:16 -0400

    landing page styling update

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -5,11 +5,14 @@ import Paw from '../services/Paw.png'
 export default function Landing() {
   return (
     <div className='landing'>
-      <img src={Paw} />
-      <p className='wufph'>wufph.</p>
-      <p>A new way to connect with Datadog
-       <br/>- worldwide
-      </p>
+        <img className="paw-image" src={Paw} />
+
+      <div className="container-text">
+        <p className='wufph'>wufph.</p>
+        <p className="connect-text">A new way to connect with Datadog 
+          <br />- <span className="purple-text"> worldwide</span> 
+        </p>
+      </div>
     </div>
   )
 }
